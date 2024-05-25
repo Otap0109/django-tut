4c0e74f65537c45edc79a6a17f55abeafc07471d
Ostap Prusik <ostapprusik@gmail.com>
2024-05-25 11:27:00 +0200

    auth

--- artemgeiblog/artemgeiblog/settings.py
+++ artemgeiblog/artemgeiblog/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mainPage.apps.MainpageConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -134,5 +136,11 @@ REST_FRAMEWORK ={
     'DEFAULT_RENDERER_CLASSES':[
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
--- artemgeiblog/mainPage/urls.py
+++ artemgeiblog/mainPage/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from mainPage import admin
 from mainPage.views import *
@@ -15,5 +15,8 @@ urlpatterns = [
     path('addArticle/', AddArticle.as_view(), name= 'addArticle'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name ='post'),
     path('category/<slug:cat_slug>/', ShowCategory.as_view(), name ='category'),
+    path('api/v1/sesauth/', include('rest_framework.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
     path('api/v1/', include(router.urls)),
     ]
